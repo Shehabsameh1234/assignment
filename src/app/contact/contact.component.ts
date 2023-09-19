b2e3caa72ba3955d03f9da00c38eef9142a2aa1d
shehabsameh1234 <shehabsameh987123@gmail.com>
2023-09-19 14:25:27 +0300

    update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -14,9 +14,21 @@ export class ContactComponent {
     titleService.setTitle("contact")
   }
 
-  label: string = ""
+  labelval: string = ""
   label1(event: any) {
-    this.label = event.target.value
+    this.labelval = event.target.value
+  }
+  labelval1: string = ""
+  label2(event: any) {
+    this.labelval1 = event.target.value
+  }
+  labelval2: string = ""
+  label3(event: any) {
+    this.labelval2 = event.target.value
+  }
+  labelval3: string = ""
+  label4(event: any) {
+    this.labelval3 = event.target.value
   }
 
 
